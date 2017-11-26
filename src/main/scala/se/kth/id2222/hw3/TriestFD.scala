cbf498a27add3acfa3ac6ab9f982fbde98fbf433
RubbyRedson <gurnik2009@gmail.com>
2017-11-26 18:13:25 +0100

    Edge deletions in FD

--- src/main/scala/se/kth/id2222/hw3/TriestFD.scala
+++ src/main/scala/se/kth/id2222/hw3/TriestFD.scala
@@ -1,5 +1,7 @@
 package se.kth.id2222.hw3
 
+import java.math.BigInteger
+
 import org.apache.flink.api.scala._
 
 import scala.collection.mutable
@@ -25,20 +27,20 @@ object TriestFD {
 
     // Reservoir sampling
     val rnd = new Random()
-    val s = 50 // Sample size
+    val s = 2000 // Sample size
     var n = 0 // How many we have encountered
 
 
     //Triest
-    var counters = new mutable.HashMap[String, BigInt]()
-    var globalEstimate: BigInt = 0
+    var counters = new mutable.HashMap[String, BigInteger]()
+    var globalEstimate: BigInteger = BigInteger.ZERO
     val graph = new mutable.HashMap[String, mutable.HashSet[String]]()
 
     var sTr = 0
     var di = 0
     var d0 = 0
 
-    val sample = new mutable.ArrayBuffer[String]()
+    var sample = new mutable.ArrayBuffer[String]()
     counts.map { edge =>
       n += 1
       sTr += 1
@@ -53,7 +55,13 @@ object TriestFD {
           }
           else {
             if (s.toDouble / n > rnd.nextDouble()) {
-              sample.remove(rnd.nextInt(sample.size))
+              val edgeToRemove = rnd.nextInt(sample.size)
+
+              val newEstimates = updateCountersEdgeDeletion(sample(edgeToRemove), counters, globalEstimate, graph, n, sample.size, di, d0)
+              globalEstimate = newEstimates._1
+              counters = newEstimates._2
+
+              sample.remove(edgeToRemove)
               sample.append(edge)
             }
           }
@@ -63,11 +71,11 @@ object TriestFD {
           d0 -= 1
         }
       } else if (counters.contains(edge.split(" ")(0).trim) && counters.contains(edge.split(" ")(1).trim)) {
-        // update counters with remove edge
-        val newEstimates = updateCounters(edge, counters, globalEstimate, graph, n, sample.size, di, d0)
+
+        val newEstimates = updateCountersEdgeDeletion(edge, counters, globalEstimate, graph, n, sample.size, di, d0)
         globalEstimate = newEstimates._1
         counters = newEstimates._2
-        //remove edge from sample
+        sample = sample.filter(el => el.ne(edge))
         di += 1
       } else {
         d0 += 1
@@ -82,9 +90,9 @@ object TriestFD {
   }
 
   // Triest FD
-  def updateCounters(newEdge: String, counters: mutable.HashMap[String, BigInt],
-                     globalEstimate: BigInt, graph: mutable.HashMap[String, mutable.HashSet[String]],
-                     n: Int, m: Int, di: Int, d0: Int): (BigInt, mutable.HashMap[String, BigInt]) = {
+  def updateCounters(newEdge: String, counters: mutable.HashMap[String, BigInteger],
+                     globalEstimate: BigInteger, graph: mutable.HashMap[String, mutable.HashSet[String]],
+                     n: Int, m: Int, di: Int, d0: Int): (BigInteger, mutable.HashMap[String, BigInteger]) = {
     val edges = newEdge.split(" ")
     val (node1, node2) = (edges(0).trim, edges(1).trim)
     var newGlobal = globalEstimate
@@ -108,30 +116,73 @@ object TriestFD {
     val neigbourhood = neighbours1.intersect(neighbours2)
 
     if (m > 2 && n > 2) {
-      var coeff = (n - 1) / (m - 1) * (n - 2) / m // compute the nu (weighted increase)
-      if (coeff < 1) coeff = 1
+      var coeff : BigInteger = new BigInteger((n - 1).toString).multiply(new BigInteger((n - 2).toString))
+        .divide(new BigInteger((m).toString)).divide(new BigInteger((m-1).toString))  // compute the nu (weighted increase)
+      coeff = coeff.max(BigInteger.ONE)
+
 
       neigbourhood.foreach(mutualNeighbour => {
-        newGlobal += coeff
+        newGlobal = newGlobal.add(coeff)
         if (counters.contains(mutualNeighbour)) {
-          counters(mutualNeighbour) = counters(mutualNeighbour) + coeff
+          counters(mutualNeighbour) = counters(mutualNeighbour).add(coeff)
         } else {
           counters(mutualNeighbour) = coeff
         }
 
         if (counters.contains(node1)) {
-          counters(node1) = counters(node1) + coeff
+          counters(node1) = counters(node1).add(coeff)
         } else {
           counters(node1) = coeff
         }
 
         if (counters.contains(node2)) {
-          counters(node2) = counters(node2) + coeff
+          counters(node2) = counters(node2).add(coeff)
         } else {
           counters(node2) = coeff
         }
       })
     }
+    (newGlobal, counters)
+  }
+
+  def updateCountersEdgeDeletion(newEdge: String, counters: mutable.HashMap[String, BigInteger],
+                                 globalSeen: BigInteger, graph: mutable.HashMap[String, mutable.HashSet[String]],
+                                 n: Int, m: Int, di: Int, d0: Int): (BigInteger, mutable.HashMap[String, BigInteger]) = {
+    val edges = newEdge.split(" ")
+    val (node1, node2) = (edges(0).trim, edges(1).trim)
+    var newGlobal = globalSeen
+
+    val neighbours1 = graph(node1)
+    val neighbours2 = graph(node2)
+    val neigbourhood = neighbours1.intersect(neighbours2)
+    if (m > 2 && n > 2) {
+      var coeff : BigInteger = new BigInteger((n - 1).toString).multiply(new BigInteger((n - 2).toString))
+        .divide(new BigInteger((m).toString)).divide(new BigInteger((m-1).toString))  // compute the nu (weighted increase)
+      coeff = coeff.max(BigInteger.ONE)
+
+      neigbourhood.foreach(mutualNeighbour => {
+        newGlobal = newGlobal.subtract(coeff)
+        if (counters.contains(mutualNeighbour)) {
+          counters(mutualNeighbour) = counters(mutualNeighbour).subtract(coeff)
+          if (counters(mutualNeighbour).equals(BigInteger.ZERO)) counters.remove(mutualNeighbour)
+        }
+        if (counters.contains(node1)) {
+          counters(node1) = counters(node1).subtract(coeff)
+          if (counters(node1).equals(BigInteger.ZERO)) counters.remove(node1)
+        }
+        if (counters.contains(node2)) {
+          counters(node2) = counters(node2).subtract(coeff)
+          if (counters(node2).equals(BigInteger.ZERO)) counters.remove(node2)
+        }
+      })
+    }
+
+    graph(node1).remove(node2)
+    graph(node2).remove(node1)
+
+    if (graph(node1).isEmpty) graph.remove(node1)
+    if (graph(node2).isEmpty) graph.remove(node2)
+
     (newGlobal, counters)
   }
 }
